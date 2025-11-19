426f4a64e74ff688883b55618a0656f2bfebe268
Artarthur <arthurbarcelos192@gmail.com>
2025-11-19 15:14:18 -0300

    servidor.js

--- servidor.js
+++ servidor.js
@@ -6,9 +6,9 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 let votos = {
-    Matematica:0,
-    Portugues:0,
-    Ciencias:0,
+    Carapebus:0,
+    Quissama:0,
+    Macae:0,
 }
 app.get("/", (req,res) => {
     res.render('form')
@@ -23,6 +23,27 @@ app.post("/voto", (req,res)=> {
 
 })
 
+app.get("/conclusao",(res,req)=> {
+    const total = votos.Carapebus + votos.Macae + votos.Quissama
+
+    res.render("conclusao",{
+        votos: votos,
+        total: total
+    })
+
+})
+
+app.get("/reiniciar",(res,req)=>{
+    votos = {
+        Carapebus:0,
+        Quissama:0,
+        Macae:0,
+    }
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Página não encontrada.</h1>");
+});
 app.listen(port, () =>{
     console.log('servidor funcioonando na porta: ${port} ')
 })
